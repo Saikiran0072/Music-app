da1f6185e18349d5c7388ceb9b0a15bd90ed8cb8
damselindistress46 <92791271+damselindistress46@users.noreply.github.com>
2021-10-22 21:38:00 +0530

    Color constants
    This file gives a detailed description of the colors that we're gonna use across pages in our app.  Each line of code is commented out for your convenience.

---
+++ colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const scaffoldbgcolor = Color(0xFF1F2933); //use for scaffold background color
+const appbarcolor = Color(0xFF323F4B); //use for appbar color
+const darkfontcolor = Color(0xFFF5F7FA);//use when background color is a light shade
+const lightfontcolor = Color(0xFF1F2933);// use when background color is a dark shade
+const searcbarcolor = Color(0xFF9AA5B1);// use for search bar
+const iconcolor = Color(0xFF323F4B);// use for all icons except for the icon(3 dashes) and navigation bar icons
+const containercolor = Color(0xFF3E4C59);// use for container
+const stackedcontainercolor = Color(0xFF52606D);//use for container when stacked upon another container
+const naviationbarcolor = Color(0xFF1CDFCB);// use for navigation bar(hold for now)
+const textboxcolor = Color(0xFF9AA5B1);//use for text boxes
+const hamburgermenuiconcolor = Color(0xFF9AA5B1);// use for hamburger menu icon
+const playmusiccolor = Color(0xFF3E4C59); // use for play music bar that pops up when a song is playing
