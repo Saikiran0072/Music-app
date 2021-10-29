1d531cc190781e7f34617ea6c742e287255a42b7
damselindistress46 <92791271+damselindistress46@users.noreply.github.com>
2021-10-30 01:29:00 +0530

    Update colors.dart

--- colors.dart
+++ colors.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 const scaffoldbgcolor = Color(0xFF1F2933); //use for scaffold background color
 const appbarcolor = Color(0xFF323F4B); //use for appbar color
-const darkfontcolor = Color(0xFFF5F7FA);//use when background color is a light shade
-const lightfontcolor = Color(0xFF1F2933);// use when background color is a dark shade
+const darkfontcolor = Color(0xFFF5F7FA);//use when background color is a dark shade
+const lightfontcolor = Color(0xFF1F2933);// use when background color is a light shade
 const searcbarcolor = Color(0xFF9AA5B1);// use for search bar
 const iconcolor = Color(0xFF323F4B);// use for all icons except for the icon(3 dashes) and navigation bar icons
-const containercolor = Color(0xFF52606D);// use for container
-const navigationbarcolor = Color(0xFF3E4C59);// use for navigation bar(hold for now)
-const textboxcolor = Color(0xFF9AA5B1);//use for text boxes
+const containercolor = Color(0xFF52606D); // use for light colored container
+const naviationbarcolor = Color(0xFF3E4C59);// use for navigation bar(hold for now)
+const lighttextboxcolor = Color(0xFF9AA5B1);//use for light colored text boxes
+const darktextboxcolor = Color(0xFF3E4C59);//use it for dark colored text boxes
 const hamburgermenuiconcolor = Color(0xFF9AA5B1);// use for hamburger menu icon
 const playmusiccolor = Color(0xFF3E4C59); // use for play music bar that pops up when a song is playing
 const navigationbariconcolor = Color(0xFF1CDFCB);
-
+const arrowcolor = Color(0xFF1CDFCB);
