4a283221afea0fd68b02e15d1f664940fab0d794
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-10-29 14:42:52 +0530

    Update options.dart

--- lib/options.dart
+++ lib/options.dart
@@ -20,7 +20,7 @@ class Options extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF3E4C59),
+      backgroundColor: scaffoldbgcolor,
       body:
 
 
