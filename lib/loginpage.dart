e75870c9e9f71aaa472f406a296683a8cde2b386
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-11-04 20:38:36 +0530

    login with firebase updated

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,6 +1,5 @@
 
 import 'package:e_commerce/Song.dart';
-import 'package:e_commerce/trypage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
@@ -24,6 +23,7 @@ void main() {
   ));
 }
 
+
 class Mainpage extends StatefulWidget {
 
 
@@ -33,10 +33,22 @@ class Mainpage extends StatefulWidget {
 
 class _MainpageState extends State<Mainpage> {
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
+
   final  _auth = FirebaseAuth.instance;
   String email= '';
   String password ='';
+  String loggedInUser = '';
 
+  void getCurrentUser() async{
+    final user = await _auth.currentUser;
+    if (user != null){
+      loggedInUser = user as String;
+      print(user.email);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Mainpagee()));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,103 +87,117 @@ class _MainpageState extends State<Mainpage> {
               SizedBox(height:80,),
 
 
-              Column(
-                children:<Widget> [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children:<Widget> [
 //email text field
-                  Container(
+                    Container(
 
 
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.only(bottom: 30),
-                        prefixIcon: Icon(Icons.email),
-                        hintText: 'Email',fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)
-                        ),
-
-                        filled: true,
-                        labelStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,color: Colors.black
-                        ),
-                      ),
-                      onChanged: (value) {
-                        email = value;
-
-
-                      },
-                      cursorColor: Colors.black,
-
-
-
-                    ),
-
-
-
-                  ),
-                  SizedBox(height: 30),
-//password text field
-                  Container(
-                    child: TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.only(bottom: 30),
-                        prefixIcon: Icon(Icons.password_outlined),
-                        hintText: 'Password',fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)
-                        ),
-                        filled: true,
-                        labelStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,color: Colors.black
-                        ),
-                      ),
-                      cursorColor: Colors.black,
-                      onChanged: (value) {
-                        password = value;
-
-                      },
-
-                    ),
-
-
-
-                  ),
-                  SizedBox(height: 80),
-                  Column(
-                    children: [
-                      Center(
-                        child: MaterialButton(
-
-                          onPressed: ()  {
-
-                          },
-
-                          child: Text("Login",style: TextStyle(fontSize: 20),),
-                          padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-                          color: Colors.white70,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),),
-                      ),
-                      MaterialButton(
-                        onPressed: () async {
-                          try {
-                            final user1 = await _auth.signInWithEmailAndPassword(
-
-                            email: email, password: password);
-                            print('successful');
-                            if (user1 != null) {
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=>Song()));} }
-                              catch (e) {
-                            print('SATVIKAAAAAAAAA');
-                            print(e);
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty ){
+                            return "*Required";
                           }
+                        },
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(bottom: 30),
+                          prefixIcon: Icon(Icons.email),
+                          hintText: 'Email',fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)
+                          ),
+
+                          filled: true,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,color: Colors.black
+                          ),
+                        ),
+                        onChanged: (value) {
+                          email = value;
+
 
                         },
-                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>Signuppage()));},
-                        child: Center(
-                          child: Center(
+                        cursorColor: Colors.black,
+
+
+
+                      ),
+
+
+
+                    ),
+                    SizedBox(height: 30),
+//password text field
+                    Container(
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "*Required";
+                          }
+                          if(value.length <6){
+                              return "this field requires atleast 6 characters";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(bottom: 30),
+                          prefixIcon: Icon(Icons.password_outlined),
+                          hintText: 'Password',fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)
+                          ),
+                          filled: true,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,color: Colors.black
+                          ),
+                        ),
+                        cursorColor: Colors.black,
+                        onChanged: (value) {
+                          password = value;
+
+                        },
+
+                      ),
+
+
+
+                    ),
+                    SizedBox(height: 80),
+                    Column(
+                      children: [
+                        Center(
+                          child: MaterialButton(
+
+                            onPressed: () async{
+                              setState(() {
+                                if (_formKey.currentState!.validate()) {
+                                  _autoValidate = true;
+                                }
+                              });
+                              try {
+                                final user1 = await _auth.signInWithEmailAndPassword(email: email, password: password);
+                                print('successful');
+                                if (user1 != null) {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Mainpagee()));} }
+                              catch (e) {
+                                print(e);
+                              }
+                            },
+                            child: Text("Login",style: TextStyle(fontSize: 20),),
+                            padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                            color: Colors.white70,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),),
+                        ),
+                        SizedBox(height: 10,),
+                        Center(
+                          child: GestureDetector(
+                            onTap: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>Signuppage()));
+                            },
                             child: Text(" Don't Have an account? ",
                               style: TextStyle(
                                   color: darkfontcolor,
@@ -184,11 +210,11 @@ class _MainpageState extends State<Mainpage> {
                           ),
 
                         ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    ),
 
-                ],
+                  ],
+                ),
               ),
             ],
           ),
