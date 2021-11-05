8f92bb9a01c7f3b436ca0112325b83dffde5989d
i-like-sahiti <92791275+i-like-sahiti@users.noreply.github.com>
2021-11-05 14:04:18 +0530

    Update signuppage.dart

--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -9,6 +9,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:e_commerce/Mainpage.dart';
+import 'loading.dart';
 
 final  _auth = FirebaseAuth.instance;
 
@@ -29,6 +30,7 @@ class _SignuppageState extends State<Signuppage> {
   bool _autoValidate = false;
 
   final  _auth = FirebaseAuth.instance;
+  bool loading = false;
 
   String fullName ='';
 
@@ -40,55 +42,55 @@ class _SignuppageState extends State<Signuppage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: scaffoldbgcolor,
+    return loading ? Loading(): Scaffold(
+      backgroundColor: scaffoldbgcolor,
 
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
 
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
 
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 100),
-                ),
-                SizedBox(height:30,),
-                //title texts
-                Center(
-                  child: Text('Sign Up', style: TextStyle(
-                      fontSize: 30, fontWeight: FontWeight.bold,
-                      color: darkfontcolor
-                  ),),
-                ),
-                Center(
-                  child: Text('Create your account', style: TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold,
-                      color: Colors.white70
-                  ),),
-                ),
-                SizedBox(height:70,),
-
-
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children:<Widget> [
-                      //full name text field
-                      Container(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 100),
+              ),
+              SizedBox(height:30,),
+              //title texts
+              Center(
+                child: Text('Sign Up', style: TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold,
+                    color: darkfontcolor
+                ),),
+              ),
+              Center(
+                child: Text('Create your account', style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.bold,
+                    color: Colors.white70
+                ),),
+              ),
+              SizedBox(height:70,),
 
 
-                        child: TextFormField(
-                          validator: (value){
-                            if(value!.isEmpty){
-                              return "*Required";
-                            }
-                            if(value.contains(RegExp(r'[0-9]'))){
-                              return "This field takes in only characters";
-                            }
-                          },
-                          decoration: InputDecoration(
+              Form(
+                key: _formKey,
+                child: Column(
+                  children:<Widget> [
+                    //full name text field
+                    Container(
+
+
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "*Required";
+                          }
+                          if(value.contains(RegExp(r'[0-9]'))){
+                            return "This field takes in only characters";
+                          }
+                        },
+                        decoration: InputDecoration(
                           contentPadding: EdgeInsets.only(bottom: 30),
                           prefixIcon: Icon(Icons.person),
                           hintText: 'Full Name',fillColor: Colors.white,
@@ -101,199 +103,206 @@ class _SignuppageState extends State<Signuppage> {
                               fontSize: 30,color: Colors.black
                           ),
                         ),
-                          cursorColor: Colors.black,
-                          onChanged: (value) {
-                            fullName = value;
+                        cursorColor: Colors.black,
+                        onChanged: (value) {
+                          fullName = value;
 
 
 
-                          },
-
-                        ),
-
-
+                        },
 
                       ),
-                      SizedBox(height: 20),
+
+
+
+                    ),
+                    SizedBox(height: 20),
 //email text field
-                      Container(
+                    Container(
 
 
-                        child: TextFormField(
-                          validator: (value){
-                            if(value!.isEmpty){
-                              return "*Required";
-                            }
-                          },
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(bottom: 30),
-                            prefixIcon: Icon(Icons.email),
-                            hintText: 'Email',fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30)
-                            ),
-                            filled: true,
-                            labelStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30,color: Colors.black
-                            ),
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "*Required";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(bottom: 30),
+                          prefixIcon: Icon(Icons.email),
+                          hintText: 'Email',fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)
                           ),
-                          cursorColor: Colors.black,
-                          onChanged: (value) {
-                            email = value;
-                          },
-
+                          filled: true,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,color: Colors.black
+                          ),
                         ),
-
-
+                        cursorColor: Colors.black,
+                        onChanged: (value) {
+                          email = value;
+                        },
 
                       ),
-                      SizedBox(height: 20),
-                      //password text field
-                      Container(
 
 
-                        child: TextFormField(
-                          validator: (value){
-                            if(value!.isEmpty){
-                              return "*Required";
-                            }
-                            if(value.length<6){
-                              return "This field needs atleast 6 characters";
-                            }
-                          },
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(bottom: 30),
-                            prefixIcon: Icon(Icons.password),
-                            hintText: 'Password',fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30)
-                            ),
-                            filled: true,
-                            labelStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30,color: Colors.black
-                            ),
+
+                    ),
+                    SizedBox(height: 20),
+                    //password text field
+                    Container(
+
+
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "*Required";
+                          }
+                          if(value.length<6){
+                            return "This field needs atleast 6 characters";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(bottom: 30),
+                          prefixIcon: Icon(Icons.password),
+                          hintText: 'Password',fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)
                           ),
-                          cursorColor: Colors.black,
-                          onChanged: (value) {
-                            setState(() {
-                              password = value;
-                            });
-
-                          },
-
+                          filled: true,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,color: Colors.black
+                          ),
                         ),
+                        cursorColor: Colors.black,
+                        onChanged: (value) {
+                          setState(() {
+                            password = value;
+                          });
 
-
+                        },
 
                       ),
-                      SizedBox(height:20.0,),
-                      //confirm password text field
-                      Container(
 
 
-                        child: TextFormField(
-                          validator: (value){
-                            if(value!.isEmpty){
-                              return "*Required";
-                            }
-                            if(value.length<6){
-                              return "This field needs atleast 6 characters";
-                            }
-                            if(password != confirm_password){
-                              return "Password doesn't match";
-                            }
-                          },
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(bottom: 30),
-                            prefixIcon: Icon(Icons.password_sharp),
-                            hintText: 'Confirm password',fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30)
-                            ),
-                            filled: true,
-                            labelStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30,color: Colors.black
-                            ),
+
+                    ),
+                    SizedBox(height:20.0,),
+                    //confirm password text field
+                    Container(
+
+
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "*Required";
+                          }
+                          if(value.length<6){
+                            return "This field needs atleast 6 characters";
+                          }
+                          if(password != confirm_password){
+                            return "Password doesn't match";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(bottom: 30),
+                          prefixIcon: Icon(Icons.password_sharp),
+                          hintText: 'Confirm password',fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)
                           ),
-                          cursorColor: Colors.black,
-                          onChanged: (value) {
-                            setState(() {
-                              confirm_password = value;
-                            });
-                          },
-
+                          filled: true,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,color: Colors.black
+                          ),
                         ),
-
-
+                        cursorColor: Colors.black,
+                        onChanged: (value) {
+                          setState(() {
+                            confirm_password = value;
+                          });
+                        },
 
                       ),
-                      SizedBox(height: 20.0,),
 
-                      SizedBox(height: 40),
+
+
+                    ),
+                    SizedBox(height: 20.0,),
+
+                    SizedBox(height: 40),
 // next arrow
-                      Center(
-                          child: Container(
-                            height: 60,
-                            width: 60,
-                            child: IconButton(
+                    Center(
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          child: IconButton(
 
-                              onPressed: () async {
-                                setState(() {
-                                  if (_formKey.currentState!.validate()) {
-                                    _autoValidate = true;
-                                  }
-                                });
-                                if(password == confirm_password){
-                                  try {
-                                    final user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                                    print('successful');
-                                    if (user != null) {
-                                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Mainpagee()));
-                                    } }
-                                  catch (e) {
-                                    print(e);
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(
-                                          content: Text('An account is already associated with this email', style: TextStyle(color: lightfontcolor),),
-                                          backgroundColor: darkfontcolor,
-                                        )
-                                    );
-                                  }
+                            onPressed: () async {
+                              setState(() {
+                                if (_formKey.currentState!.validate()) {
+                                  _autoValidate = true;
                                 }
-                                else{
+                              });
+                              setState(() {
+                                loading =true;
+
+                              });
+                              if(password == confirm_password){
+                                try {
+                                  final user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                                  print('successful');
+                                  if (user != null) {
+                                    setState(() {
+                                      loading =false;
+                                    });
+                                    Navigator.push(context, MaterialPageRoute(builder: (context)=>Mainpagee()));
+                                  } }
+                                catch (e) {
+                                  print(e);
                                   ScaffoldMessenger.of(context).showSnackBar(
                                       SnackBar(
-                                        content: Text('Try Again! The Passwords dont match', style: TextStyle(color: lightfontcolor),),
+                                        content: Text('An account is already associated with this email', style: TextStyle(color: lightfontcolor),),
                                         backgroundColor: darkfontcolor,
-
                                       )
                                   );
                                 }
-                              } ,
-                              color: Colors.white,
-                              icon: const Icon(Icons.arrow_forward_ios_sharp, color:arrowcolor),
+                              }
+                              else{
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text('Try Again! The Passwords dont match', style: TextStyle(color: lightfontcolor),),
+                                      backgroundColor: darkfontcolor,
+
+                                    )
+                                );
+                              }
+                            } ,
+                            color: Colors.white,
+                            icon: const Icon(Icons.arrow_forward_ios_sharp, color:arrowcolor),
 
 
 
-                            ),
+                          ),
 
-                          )
+                        )
 
-                      ),
-                      SizedBox(height: 30),
+                    ),
+                    SizedBox(height: 30),
 
-                    ],
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
+      ),
     );
 
 
