c8b3e1c3358928089cdd6f4749f3c8e34a00fd82
damselindistress46 <92791271+damselindistress46@users.noreply.github.com>
2021-11-06 20:05:28 +0530

    Create insertdata.php

---
+++ insertdata.php
@@ -0,0 +1,20 @@
+<?php 
+	include "test-connect.php";
+	// REGISTER USER
+	echo 'H E L L O';
+	echo 'gfgchvhyvhccgcgcgjcgcg'
+
+	  
+	    $Artist_id = mysqli_real_escape_string($connect, $_POST['Artist_id']);
+	    $followers = mysqli_real_escape_string($connect, $_POST['followers']);
+	   
+	  
+	 
+	        $query = "INSERT INTO artist (Artist_id, followers)
+	  			  VALUES('$Artist_id', '$followers')";
+	    $results = mysqli_query($connect, $query);
+	    if($results>0)
+	    {
+		echo "user added successfully"; }
+			?>
+			
