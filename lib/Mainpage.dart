d6d6f98950b11b97699ae241f357e64ec1d1fb96
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-11-08 18:19:46 +0530

    Added banners

--- lib/Mainpage.dart
+++ lib/Mainpage.dart
@@ -38,7 +38,7 @@ class Mainpagee extends StatelessWidget {
         leading: IconButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Options()));},
           icon: Icon(
             Icons.menu,
-            color: arrowcolor,
+            color: Color(0xFF9AA5B1),
 
           ),),
         actions: [
@@ -63,147 +63,135 @@ class Mainpagee extends StatelessWidget {
 
 
       body: SingleChildScrollView(
-      child: SafeArea(
+        child: SafeArea(
 
-         child:Column(
-           children: [
+          child:Column(
+            children: [
 
               SizedBox(height: 30.0,),
 //search bar
               Container(
-                  child: Neumorphic(
-                    margin: EdgeInsets.fromLTRB(30.0, 0  , 30, 0),
-                     child: TextField(
-                       decoration: InputDecoration(
-                       icon: Icon(Icons.search),
-                       hintText:'Search for ...',
-                       enabledBorder: UnderlineInputBorder(
-                       borderSide: BorderSide(color: Color(0xFF9AA5B1)),
-                          ),
-                       fillColor: Color(0xFF9AA5B1),
-                       filled: true,
-                       border: UnderlineInputBorder(),
-                       labelStyle: TextStyle(
-                       fontWeight: FontWeight.bold,
-                       fontSize: 30,
-                       color: Color(0xFFF5F7FA),
-                                ),
-                       ),
-                     cursorColor: Colors.black,
-                          ),
-                     style: NeumorphicStyle(
-                     shape: NeumorphicShape.concave,
-                     boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
-                     depth: -8,
-                     lightSource: LightSource.topLeft,
-                     color: Color(0xFF9AA5B1),
-                           ),
+                child: Neumorphic(
+                  margin: EdgeInsets.fromLTRB(30.0, 0  , 30, 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.search),
+                      hintText:'Search for ...',
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF9AA5B1)),
+                      ),
+                      fillColor: Color(0xFF9AA5B1),
+                      filled: true,
+                      border: UnderlineInputBorder(),
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Color(0xFFF5F7FA),
+                      ),
+                    ),
+                    cursorColor: Colors.black,
                   ),
+                  style: NeumorphicStyle(
+                    shape: NeumorphicShape.concave,
+                    boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
+                    depth: -8,
+                    lightSource: LightSource.topLeft,
+                    color: Color(0xFF9AA5B1),
+                  ),
+                ),
               ),
               SizedBox(height: 30.0,),
 
- //top containers
+              //top containers
               SingleChildScrollView(
-                 scrollDirection: Axis.horizontal,
-                 child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                       SizedBox(width: 10,),
-                       Stack(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.all(10),
-                                height: 150.0,
-                                width: 350.0,
-                                decoration: BoxDecoration(
-                                   borderRadius: BorderRadius.circular(50),
-                                   color: containercolor,
-                                ),
-                                child: Image.asset(''),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    SizedBox(width: 10,),
+                    Stack(
+                      children: [
+                        Container(
+                          height: 150.0,
+                          width: 350.0,
+                          color: containercolor,
+                          child: Image.asset('images/banner.jpg', fit: BoxFit.fill,),
 
-                            ),
-                          ],
-                       ),
-                       SizedBox(width:15,),
-                       Stack(
-                          children: [
-                             Container(
-                                 padding: EdgeInsets.all(10),
-                                 height: 150.0,
-                                 width: 350.0,
-                                 decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    color: containercolor,
-                                 ),
-                                 child: Image.asset('assets/4.png'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width:15,),
+                    Stack(
+                      children: [
+                        Container(
+                          height: 150.0,
+                          width: 350.0,
+                          color: containercolor,
+                          child: Image.asset('images/banner.jpg', fit: BoxFit.fill,),
 
-                             ),
-                          ],
-                       ),
-                       SizedBox(width:15,),
-                       Stack(
-                          children: [
-                             Container(
-                                 padding: EdgeInsets.all(10),
-                                 height: 150.0,
-                                  width: 350.0,
-                                  decoration: BoxDecoration(
-                                     borderRadius: BorderRadius.circular(50),
-                                     color: containercolor,
-                                  ),
-                                  child: Image.asset('assets/4.png'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width:15,),
+                    Stack(
+                      children: [
+                        Container(
+                          height: 150.0,
+                          width: 350.0,
+                          color: containercolor,
+                          child: Image.asset('images/banner.jpg', fit: BoxFit.fill,),
 
-                             ),
-                          ],
-                       ),
-                       SizedBox(width:15,),
-                    ],
-                 ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width:15,),
+                  ],
+                ),
               ),
 
 
-    //categories and view all buttons
+              //categories and view all buttons
               Row(
                 children: [
                   SizedBox(height: 120.0,),
                   Container(
-                      margin: EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
-                      child: Icon(Icons.format_list_bulleted_sharp,
-                        color: arrowcolor,
+                    margin: EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+                    child: Icon(Icons.format_list_bulleted_sharp,
+                      color: Color(0xFF9AA5B1),
 
-                      ),
+                    ),
                   ),
                   Container(
-                      padding: EdgeInsets.only(left: 20.0),
-                      child: Text('Categories',
-                        style: TextStyle(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text('Categories',
+                      style: TextStyle(
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold,
                         color: Color(0xFFF5F7FA),
 
-                         ),
                       ),
-                   ),
+                    ),
+                  ),
                   SizedBox(width:60,),
                   Container(
-                      child:Expanded(
-                         child: RaisedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Categories()));},
-                           child: Text('View All',
-                             style: TextStyle(
-                               fontSize: 17,
-                               fontWeight: FontWeight.bold,
-                               color: Color(0xFFF5F7FA),), ),
-                           color: Color(0xFF52606D),
-                           padding: EdgeInsets.only(top:10,bottom: 10),
-                         ),
+                    child:Expanded(
+                      child: RaisedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Categories()));},
+                        child: Text('View All',
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFF5F7FA),), ),
+                        color: Color(0xFF52606D),
+                        padding: EdgeInsets.only(top:10,bottom: 10),
                       ),
+                    ),
                     padding: EdgeInsets.only(left: 40),
                   ),
                 ],
               ),
 
 
-    // genres
+              // genres
               SingleChildScrollView(scrollDirection: Axis.horizontal,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -433,69 +421,69 @@ class Mainpagee extends StatelessWidget {
               ),
 
 //playmusic bar(mad love)
-             SizedBox(height: 50.0,),
-             Container(
-               color: playmusiccolor,
-               child: Row(
-                 children: [
-                   Container(
-                     padding: EdgeInsets.only(left: 20.0),
-                     height: 70.0,
-                     width: 50.0,
-                     decoration: BoxDecoration(
-                       shape: BoxShape.circle,
-                       image: DecorationImage(
-                         fit: BoxFit.cover,
-                         image: AssetImage('images/default_img.jpg',),
-                       ),
-                       border: Border.all(
-                         width: 4,
-                         color: Color(0xFF9AA5B1),
-                       ),
-                     ),
-                   ),
-                   Padding(
-                     padding: EdgeInsets.only(left: 10.0),
-                     child: TextButton(
-                       onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Song()));},
-                       child: Text('Infinity',
-                         style: TextStyle(
+              SizedBox(height: 50.0,),
+              Container(
+                color: playmusiccolor,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(left: 20.0),
+                      height: 70.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('images/default_img.jpg',),
+                        ),
+                        border: Border.all(
+                          width: 4,
+                          color: Color(0xFF9AA5B1),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: TextButton(
+                        onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Song()));},
+                        child: Text('Infinity',
+                          style: TextStyle(
 
-                           fontSize: 18.0,
-                           fontWeight: FontWeight.bold,
-                           color: Color(0xFFF5F7FA),
-                         ),
-                       ),
-                     ),
-                   ),
-                   Expanded(child: Padding(
-                     padding: const EdgeInsets.only(left: 70.0),
-                     child: Icon(Icons.arrow_left,color: arrowcolor,),
-                   ),
-                   ),
-                   Expanded(child:
-                   Padding(
-                     padding: const EdgeInsets.only(left: 50.0),
-                     child: Icon(Icons.pause,color: arrowcolor,),
-                   ),
-                   ),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFF5F7FA),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(child: Padding(
+                      padding: const EdgeInsets.only(left: 70.0),
+                      child: Icon(Icons.arrow_left,color: Color(0xFF9AA5B1),),
+                    ),
+                    ),
+                    Expanded(child:
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50.0),
+                      child: Icon(Icons.pause,color: Color(0xFF9AA5B1),),
+                    ),
+                    ),
 
-                   Expanded(child: Padding(
-                     padding: const EdgeInsets.only(left: 30.0),
-                     child: Icon(Icons.arrow_right,color: arrowcolor,),
-                   ),
-                   ),
-                   Expanded(child: Padding(
-                     padding: const EdgeInsets.only(left: 5.0),
-                     child: Icon(Icons.close,color: arrowcolor,),
-                   ),
-                   ),
-                 ],
-               ),
-             ),
-           ],
-         ),
-      ),
+                    Expanded(child: Padding(
+                      padding: const EdgeInsets.only(left: 30.0),
+                      child: Icon(Icons.arrow_right,color: Color(0xFF9AA5B1),),
+                    ),
+                    ),
+                    Expanded(child: Padding(
+                      padding: const EdgeInsets.only(left: 5.0),
+                      child: Icon(Icons.close,color: Color(0xFF9AA5B1),),
+                    ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
 
       //bottom navigation bar
