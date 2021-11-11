a01b5a42d521533dc7f51c7c920a52943671f4e9
damselindistress46 <92791271+damselindistress46@users.noreply.github.com>
2021-11-11 11:46:46 +0530

    Update favorites.dart

--- lib/favorites.dart
+++ lib/favorites.dart
@@ -13,9 +13,20 @@ void main() {
     home: Favorites(),
   ));
 }
-class Favorites extends StatelessWidget {
+class Favorites extends StatefulWidget {
 
 
+  @override
+  State<Favorites> createState() => _FavoritesState();
+}
+
+class _FavoritesState extends State<Favorites> {
+  List<bool> _selections1 = List.generate(1, (_) => false);
+  List<bool> _selections2 = List.generate(1, (_) => false);
+  List<bool> _selections3 = List.generate(1, (_) => false);
+  List<bool> _selections4 = List.generate(1, (_) => false);
+  List<bool> _selections5 = List.generate(1, (_) => false);
+  List<bool> _selections6 = List.generate(1, (_) => false);
   void newval= iconcolor;
 
   @override
@@ -69,10 +80,7 @@ class Favorites extends StatelessWidget {
                                   child: Image.asset('images/default_img.jpg',
                                     height: 120,
                                     width: 120,
-
-
                                   ),
-
 
                                 ),
                                 Expanded(
@@ -114,9 +122,107 @@ class Favorites extends StatelessWidget {
                         ),
                       ),
                       SizedBox(width: 40,),
-                      Icon(
-                        Icons.favorite,
-                        color: navigationbariconcolor,
+                      ToggleButtons(
+                        color: navigationbarcolor,
+                        borderColor: scaffoldbgcolor,
+                        selectedColor: navigationbariconcolor,
+                        selectedBorderColor: navigationbarcolor,
+                        renderBorder: false,
+                        fillColor: scaffoldbgcolor,
+                        children: <Widget> [
+                          Icon(Icons.favorite),
+                        ],
+                        isSelected: _selections1,
+                        onPressed: (int index) {
+                          setState(() {
+                            _selections1[index] = !_selections1[index];
+
+                          });
+                        },
+
+
+
+                      ),
+                      SizedBox(width: 10,)
+                    ],
+                  ),
+                  SizedBox(height:20,),Row(
+                    children: <Widget> [
+                      SizedBox(
+                          width:20
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Song()));},
+                          child: Container(
+                            child: Row(
+                              children: <Widget> [
+
+                                Expanded(
+                                  child: Image.asset('images/default_img.jpg',
+                                    height: 120,
+                                    width: 120,
+                                  ),
+
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: <Widget> [
+                                      Text('song name',
+                                        style: TextStyle(
+                                          letterSpacing: 1,
+                                          color: darkfontcolor,
+                                          fontStyle: FontStyle.italic,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      SizedBox(height:2,),
+                                      Text('artist name',
+                                        style: TextStyle(
+                                          color: darkfontcolor,
+                                          fontStyle: FontStyle.italic,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+
+                                ),
+                              ],
+                            ),
+                            padding: EdgeInsets.all(20),
+                            height: 100,
+                            width: 360,
+                            decoration: BoxDecoration(
+                              color: containercolor,
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(30),
+
+                            ),
+
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 40,),
+                      ToggleButtons(
+                        color: navigationbarcolor,
+                        borderColor: scaffoldbgcolor,
+                        selectedColor: navigationbariconcolor,
+                        selectedBorderColor: navigationbarcolor,
+                        renderBorder: false,
+                        fillColor: scaffoldbgcolor,
+                        children: <Widget> [
+                          Icon(Icons.favorite),
+                        ],
+                        isSelected: _selections2,
+                        onPressed: (int index) {
+                          setState(() {
+                            _selections2[index] = !_selections2[index];
+
+                          });
+                        },
+
+
 
                       ),
                       SizedBox(width: 10,)
@@ -184,10 +290,22 @@ class Favorites extends StatelessWidget {
                         ),
                       ),
                       SizedBox(width: 40,),
-                      Icon(
-                        Icons.favorite,
-                        color: navigationbariconcolor,
-
+                      ToggleButtons(
+                        color: navigationbarcolor,
+                        borderColor: scaffoldbgcolor,
+                        selectedColor: navigationbariconcolor,
+                        selectedBorderColor: navigationbarcolor,
+                        renderBorder: false,
+                        fillColor: scaffoldbgcolor,
+                        children: <Widget> [
+                          Icon(Icons.favorite),
+                        ],
+                        isSelected: _selections3,
+                        onPressed: (int index) {
+                          setState(() {
+                            _selections3[index] = !_selections3[index];
+                          });
+                        },
                       ),
                       SizedBox(width: 10,)
                     ],
@@ -254,10 +372,22 @@ class Favorites extends StatelessWidget {
                         ),
                       ),
                       SizedBox(width: 40,),
-                      Icon(
-                        Icons.favorite,
-                        color: navigationbariconcolor,
-
+                      ToggleButtons(
+                        color: navigationbarcolor,
+                        borderColor: scaffoldbgcolor,
+                        selectedColor: navigationbariconcolor,
+                        selectedBorderColor: navigationbarcolor,
+                        renderBorder: false,
+                        fillColor: scaffoldbgcolor,
+                        children: <Widget> [
+                          Icon(Icons.favorite),
+                        ],
+                        isSelected: _selections4,
+                        onPressed: (int index) {
+                          setState(() {
+                            _selections4[index] = !_selections4[index];
+                          });
+                        },
                       ),
                       SizedBox(width: 10,)
                     ],
@@ -324,10 +454,22 @@ class Favorites extends StatelessWidget {
                         ),
                       ),
                       SizedBox(width: 40,),
-                      Icon(
-                        Icons.favorite,
-                        color: navigationbariconcolor,
-
+                      ToggleButtons(
+                        color: navigationbarcolor,
+                        borderColor: scaffoldbgcolor,
+                        selectedColor: navigationbariconcolor,
+                        selectedBorderColor: navigationbarcolor,
+                        renderBorder: false,
+                        fillColor: scaffoldbgcolor,
+                        children: <Widget> [
+                          Icon(Icons.favorite),
+                        ],
+                        isSelected: _selections5,
+                        onPressed: (int index) {
+                          setState(() {
+                            _selections5[index] = !_selections5[index];
+                          });
+                        },
                       ),
                       SizedBox(width: 10,)
                     ],
@@ -349,8 +491,6 @@ class Favorites extends StatelessWidget {
                                   child: Image.asset('images/default_img.jpg',
                                     height: 120,
                                     width: 120,
-
-
                                   ),
 
 
@@ -394,80 +534,22 @@ class Favorites extends StatelessWidget {
                         ),
                       ),
                       SizedBox(width: 40,),
-                      Icon(
-                        Icons.favorite,
-                        color: navigationbariconcolor,
-
-                      ),
-                      SizedBox(width: 10,)
-                    ],
-                  ),
-                  SizedBox(height:20,),
-                  Row(
-                    children: <Widget> [
-                      SizedBox(
-                          width:20
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Song()));},
-                          child: Container(
-                            child: Row(
-                              children: <Widget> [
-
-                                Expanded(
-                                  child: Image.asset('images/default_img.jpg',
-                                    height: 120,
-                                    width: 120,
-
-
-                                  ),
-
-
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    children: <Widget> [
-                                      Text('song name',
-                                        style: TextStyle(
-                                          letterSpacing: 1,
-                                          color: darkfontcolor,
-                                          fontStyle: FontStyle.italic,
-                                          fontSize: 15,
-                                        ),
-                                      ),
-                                      SizedBox(height:2,),
-                                      Text('artist name',
-                                        style: TextStyle(
-                                          color: darkfontcolor,
-                                          fontStyle: FontStyle.italic,
-                                          fontSize: 15,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-
-                                ),
-                              ],
-                            ),
-                            padding: EdgeInsets.all(20),
-                            height: 100,
-                            width: 360,
-                            decoration: BoxDecoration(
-                              color: containercolor,
-                              shape: BoxShape.rectangle,
-                              borderRadius: BorderRadius.circular(30),
-
-                            ),
-
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 40,),
-                      Icon(
-                        Icons.favorite,
-                        color: navigationbariconcolor,
-
+                      ToggleButtons(
+                        color: navigationbarcolor,
+                        borderColor: scaffoldbgcolor,
+                        selectedColor: navigationbariconcolor,
+                        selectedBorderColor: navigationbarcolor,
+                        renderBorder: false,
+                        fillColor: scaffoldbgcolor,
+                        children: <Widget> [
+                          Icon(Icons.favorite),
+                        ],
+                        isSelected: _selections6,
+                        onPressed: (int index) {
+                          setState(() {
+                            _selections6[index] = !_selections6[index];
+                          });
+                        },
                       ),
                       SizedBox(width: 10,)
                     ],
