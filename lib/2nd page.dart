3b8c6061c9842c2cd93f1a16216951e6aa504184
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-11-20 17:02:12 +0530

    Update 2nd page.dart

--- lib/2nd page.dart
+++ lib/2nd page.dart
@@ -8,13 +8,8 @@ import 'signuppage.dart';
 import 'colors.dart';
 
 
-void main() {
-  runApp(MaterialApp(
-    home: Login(),
-  ));
-}
-
 class Login extends StatelessWidget {
+  static const String id = "second_screen";
   const Login({Key? key}) : super(key: key);
 
   @override
@@ -32,12 +27,12 @@ class Login extends StatelessWidget {
                 SizedBox(height: 50.0,),
                 Center(
                   child: Container(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: Image.asset('images/final_logo_apppages.jpeg',
-                          height:200.0 ,
-                          width: 300.0,
-                          fit: BoxFit.fill,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Image.asset('images/final_logo_apppages.jpeg',
+                        height:200.0 ,
+                        width: 300.0,
+                        fit: BoxFit.fill,
 
 
 
@@ -49,49 +44,49 @@ class Login extends StatelessWidget {
                 ),
                 SizedBox(height: 140.0,),
                 Container(
-                    padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
                   decoration: BoxDecoration(
                     color: containercolor,
                     borderRadius: BorderRadius.circular(10),
                   ),
 
-                    child: RaisedButton(
-                      color: containercolor,
-                      onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=>Mainpage()));},
-                      child: Text('Sign In',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: darkfontcolor,
-                          )
-                      ),
-
-
+                  child: RaisedButton(
+                    color: containercolor,
+                    onPressed: () {Navigator.pushNamed(context, Mainpage.id);},
+                    child: Text('Sign In',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: darkfontcolor,
+                        )
                     ),
 
+
                   ),
+
+                ),
                 SizedBox(height: 30.0,),
                 Container(
                   padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-                    decoration: BoxDecoration(
-                      color: containercolor,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: RaisedButton(
-                      color: containercolor,
-                      onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=>Signuppage()));},
-                      child: Text('Sign Up',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: darkfontcolor,
-                          ),
+                  decoration: BoxDecoration(
+                    color: containercolor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: RaisedButton(
+                    color: containercolor,
+                    onPressed: () {Navigator.pushNamed(context, Signuppage.id);},
+                    child: Text('Sign Up',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: darkfontcolor,
                       ),
-
-
                     ),
+
 
                   ),
+
+                ),
 
 
 
