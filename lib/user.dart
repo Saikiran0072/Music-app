3309f4821a20517f12426a994937cb6229d05b13
damselindistress46 <92791271+damselindistress46@users.noreply.github.com>
2021-11-21 21:11:31 +0530

    Update user.dart

--- lib/user.dart
+++ lib/user.dart
@@ -7,9 +7,14 @@ class Data extends ChangeNotifier{
   String email = "";
   String fullname = "";
   List data_list = [];
+  String genrename='';
+  List album_datalist= [];
+  String albumname='';
+  List artist_datalist=[];
 
+  //login data
   Future getData() async{
-    http.Response response = await http.get(Uri.parse('http://192.168.0.6/select.php'),
+    http.Response response = await http.get(Uri.parse('http://192.168.0.102/select.php'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -33,4 +38,66 @@ class Data extends ChangeNotifier{
     email = data;
     notifyListeners();
   }
+  List data_list2= [];
+  String songname='';
+  int AlbumId=0;
+
+  //song data
+  Future getsongData() async{
+    http.Response response = await http.get(Uri.parse('http://192.168.0.102/songdatasample.php'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    data_list2 = jsonDecode(response.body);
+    for(int i =0;i<data_list2.length;i++) {
+      print(data_list2[i]);
+    }
+
+    print(data_list2.length);
+    print(response.statusCode);
+    print(songname);
+    print(AlbumId);
+  }
+  String get Genre => genrename;
+  set Genre(String data){
+    genrename = data;
+    notifyListeners();
+  }
+
+
+  //artist data
+  Future getartistData() async{
+    http.Response response = await http.get(Uri.parse('http://192.168.0.102/artistdatasample.php'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    artist_datalist = jsonDecode(response.body);
+
+    notifyListeners();
+    print(response.statusCode);
+  }
+  String get Album => albumname;
+  set Album(String data){
+    albumname = data;
+    notifyListeners();
+  }
+//album data
+  Future getalbumData() async{
+    http.Response response = await http.get(Uri.parse('http://192.168.0.102/albumdatasample.php'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    album_datalist = jsonDecode(response.body);
+    for(int i =0;i<data_list2.length;i++){
+      print(data_list2[i]);
+      if(data_list2[i]['AlbumId']== album_datalist[i])
+    }
+
+    notifyListeners();
+    print(response.statusCode);
+  }
 }
+
