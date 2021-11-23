439b9e09e7ecf474f5b0869999193d32eed645a9
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-11-23 19:52:16 +0530

    Update ArtistInfo.dart

--- lib/ArtistInfo.dart
+++ lib/ArtistInfo.dart
@@ -1,4 +1,3 @@
-
 import 'dart:ui';
 import 'package:e_commerce/colors.dart';
 import 'package:flutter/cupertino.dart';
@@ -28,29 +27,20 @@ class _ArtistState extends State<Artist> {
   bool _hasBeenPressed = false;
   bool _pressedtext =false;
 
-  Future senddatatodb() async {
-    print('H E L L OOOOOOOOOO');
-    http.Response response = await http.post(
-        Uri.parse('http://192.168.0.102/insertdata.php'),
+  Future updateFollow() async{
+    http.Response response = await http.post(Uri.parse('http://192.168.0.6/update_follow.php'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: json.encode({
+        body:json.encode({
 
-          "Artist_id": 1,
-          "followers": followers,
+          "followers": Provider.of<Data>(context,listen: false).followers,
+          "ArtistId": Provider.of<Data>(context, listen: false).Album
 
 
         }));
+    print("success");
     print(response.statusCode);
-  }
-
-  Future getData() async {
-    http.Response response = await http.get(
-        Uri.parse("http://192.168.0.102/insertdata.php"));
-    var data = jsonDecode(response.body);
-    print(response.statusCode);
-    print(data.toString());
   }
 
   Widget build(BuildContext context) {
@@ -74,8 +64,8 @@ class _ArtistState extends State<Artist> {
                     ),
                   ),
                   Container(
-                      width: 700,
-                      height: 200,
+                      width: 500,
+                      height: 150,
                       color: containercolor,
                       padding: EdgeInsets.only(left: 20, bottom: 20),
                       alignment: Alignment.bottomLeft,
@@ -90,7 +80,7 @@ class _ArtistState extends State<Artist> {
                                   fontSize: 35,
                                   color: darkfontcolor
                               ),),
-                            Text("xxxxx followers",
+                            Text("${Provider.of<Data>(context).followers.toString()} followers",
                               style: TextStyle(
                                 fontSize: 18,
                                 color: darkfontcolor,
@@ -105,18 +95,25 @@ class _ArtistState extends State<Artist> {
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(right: 130),
-                        child: MaterialButton(
-                          onPressed: () {
-                            setState(() {
-                              _hasBeenPressed = !_hasBeenPressed;
+                        child: MaterialButton(onPressed: () async{
+                          setState(() {
+                            _hasBeenPressed = !_hasBeenPressed;
 
-                            });
-                            setState(() {
-                              _pressedtext = !_pressedtext;
-                            });
-                            followers++;
-                            senddatatodb();
-                          },
+                          });
+                          setState(() {
+                            _pressedtext = !_pressedtext;
+                          });
+                          setState(() {
+                            if(_hasBeenPressed){
+                              Provider.of<Data>(context, listen: false).followers++;
+                            }
+                            else{
+                              Provider.of<Data>(context, listen: false).followers--;
+                            }
+
+                          });
+                          await updateFollow();
+                        },
                           child: _pressedtext ? Text("FOLLOWING",
                             style: TextStyle(
                               color: lightfontcolor,
@@ -128,12 +125,11 @@ class _ArtistState extends State<Artist> {
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-
                           color: _hasBeenPressed ? arrowcolor : darkfontcolor,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(20)),),
                       ),
-                      Icon(Icons.play_circle_fill_outlined, size: 50,
+                      Icon(Icons.play_circle_fill_outlined, size: 40,
                         color: navigationbariconcolor,)
                     ],
                   ),
@@ -167,3 +163,4 @@ class _ArtistState extends State<Artist> {
     );
   }
 }
+
