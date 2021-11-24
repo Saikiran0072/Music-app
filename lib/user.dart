d427ebba1db3d39418135b2f07542f6c44fcb9e3
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-11-24 17:03:57 +0530

    Update user.dart

--- lib/user.dart
+++ lib/user.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'package:e_commerce/Song.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Data extends ChangeNotifier{
   String email = "";
@@ -18,6 +20,13 @@ class Data extends ChangeNotifier{
   List artist_follow = [];
   String artist_name="";
   int followers =0;
+  int songid = 0;
+  int likes = 0;
+  List favList = [];
+  List buttonList = [];
+  List selectSong= ["infinity.mp3","black swan.mp3","Gimme More.mp3","good 4 u.mp3", "i wanna be your slave.mp3", "infinity.mp3","kiss me more.mp3","sweat.mp3","sweet melody.mp3"];
+  List chosenList = [];
+  final audioPlayer = AudioPlayer();
 
   //login data
   Future getData() async{
@@ -93,6 +102,24 @@ class Data extends ChangeNotifier{
     artistId = data;
     notifyListeners();
   }
+
+  int get Songid => songid;
+  set Songid(int data){
+    songid = data;
+    notifyListeners();
+  }
+
+  String get Songname => songname;
+  set Songname(String data){
+    songname = data;
+    notifyListeners();
+  }
+
+  int get Likes => likes;
+  set Likes(int data){
+    likes = data;
+    notifyListeners();
+  }
 //album data
   Future getalbumData() async{
     http.Response response = await http.post(Uri.parse('http://192.168.0.6/albumdatasample.php'),
@@ -135,6 +162,69 @@ class Data extends ChangeNotifier{
     print(response.statusCode);
     print(artist_follow);
   }
+
+  Future updateLikes() async{
+    http.Response response = await http.post(Uri.parse('http://192.168.0.6/update_likes.php'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body:json.encode({
+
+          "likes_song": Likes,
+          "SongId": Songid
+
+
+        }));
+    print("db likes updated");
+    print("success");
+    print(response.statusCode);
+  }
+  Future favSong() async{
+    favList += [{"songname" : Songname, "artistname" : ArtistName, "songid": Songid, "artist_id": artistId}];
+  }
+  Future likeButton() async{
+    if(!buttonList.contains(Songid)){
+    buttonList += [Songid];
+    }
+
+  }
+  Future unlikeButton(int songid) async{
+    for(int i =0;i<buttonList.length;i++){
+      if(buttonList[i] == songid){
+        buttonList.removeAt(i);
+      }
+    }
+  }
+
+  Future unfavSong(String songname) async{
+    for(int i =0;i<favList.length;i++){
+      if(favList[i]["songname"] == songname){
+        favList.removeAt(i);
+      }
+    }
+  }
+
+  void playSound(String sound) {
+    AudioCache cache = new AudioCache(fixedPlayer: audioPlayer);
+    cache.play(sound);
+  }
+
+  void pauseSound() {audioPlayer.pause();}
+
+  Future selectMusic(String sound) async {
+    if(!chosenList.contains(sound)){
+      chosenList += [sound];
+    }
+
+  }
+  Future unselectMusic(String sound) async {
+    for(int i =0;i<selectSong.length;i++){
+      if(selectSong[i] == sound){
+        chosenList.removeAt(i);
+      }
+    }
+  }
+
 
 }
 
