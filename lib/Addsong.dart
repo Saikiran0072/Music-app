7f02951f98c78cbbcdf58728d9e4361b0e39126b
damselindistress46 <92791271+damselindistress46@users.noreply.github.com>
2021-11-26 22:56:36 +0530

    Update Addsong.dart

--- lib/Addsong.dart
+++ lib/Addsong.dart
@@ -42,37 +42,7 @@ class Addsong extends StatelessWidget {
           children: [
 
             SizedBox(height: 30.0,),
-            Container(
-              child: Neumorphic(
-                margin: EdgeInsets.fromLTRB(30.0, 0  , 30, 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.search),
-                    hintText:'Search for ...',
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF9AA5B1)),
-                    ),
-                    fillColor: Color(0xFF9AA5B1),
-                    filled: true,
-                    border: UnderlineInputBorder(),
-                    labelStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                      color: Color(0xFFF5F7FA),
-                    ),
-                  ),
-                  cursorColor: Colors.black,
-                ),
-                style: NeumorphicStyle(
-                  shape: NeumorphicShape.concave,
-                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(30)),
-                  depth: -8,
-                  lightSource: LightSource.topLeft,
-                  color: Color(0xFF9AA5B1),
-                ),
-              ),
-            ),
-            SizedBox(height:30.0 ,),
+
             Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -91,8 +61,8 @@ class Addsong extends StatelessWidget {
                 Padding(
                   padding: EdgeInsets.only(left: 20, right: 20),
                   child: Container(
-                    alignment: Alignment.centerLeft,
-                    child: recommendedSongs(songdata: Provider.of<Data>(context,listen:false).data_list2)
+                      alignment: Alignment.centerLeft,
+                      child: recommendedSongs(songdata: Provider.of<Data>(context,listen:false).data_list2)
                   ),
                 ),
               ],
