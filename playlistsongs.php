ac2cc652ae15f001a8be2c25aeaa17f70e79262e
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-11-26 20:31:05 +0530

    Create playlistsongs.php

---
+++ playlistsongs.php
@@ -0,0 +1,48 @@
+<?php
+
+//Define your Server host name here.
+$HostName = "localhost";
+ 
+//Define your MySQL Database Name here.
+$DatabaseName = "musicdb";
+ 
+//Define your Database User Name here.
+$HostUser = "root";
+ 
+//Define your Database Password here.
+$HostPass = ""; 
+ 
+// Creating MySQL Connection.
+$con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ 
+// Storing the received JSON into $json variable.
+$json = file_get_contents('php://input');
+ 
+// Decode the received JSON and Store into $obj variable.
+$obj = json_decode($json,true);
+ 
+$data=array();
+ 
+// Getting name from $obj object.
+$SongId = $obj['SongId'] ?? "";
+
+// Getting name from $obj object.
+$PlaylistId = $obj['PlaylistId'] ?? "";
+ 
+$sql = "SELECT SongId, PlaylistId FROM playlistsongs WHERE SongId= ('$SongId')";
+
+$result = mysqli_query($con,$sql);
+$count = mysqli_num_rows($result);
+if($count){
+	echo json_encode("Error");
+}
+else{
+	$insert ="INSERT INTO playlistsongs(SongId, PlaylistId)VALUES(('$SongId'),('$PlaylistId'))";
+	$query = mysqli_query($con,$insert);
+	if($query){
+		echo json_encode("Success");
+	}
+}
+	 
+ mysqli_close($con);
+?>
