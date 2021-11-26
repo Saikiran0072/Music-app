57d99eb6998dc9fd7d337c658db27cd1d831f8c8
B. Hamsini <56066633+Hamskira@users.noreply.github.com>
2021-11-26 20:26:09 +0530

    Update catogory.dart

--- lib/catogory.dart
+++ lib/catogory.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/ArtistInfo.dart';
+import 'package:e_commerce/Extract.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
@@ -7,14 +8,11 @@ import 'package:e_commerce/savedplaylist.dart';
 import 'catogories.dart';
 import 'colors.dart';
 import 'Song.dart';
-
-
-
-
+import 'package:provider/provider.dart';
+import 'user.dart';
 
 class Category extends StatelessWidget {
   static const String id = "category_screen";
-  const Category({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +22,7 @@ class Category extends StatelessWidget {
         elevation: 0,
         leading: IconButton(
           onPressed: () {
+
             Navigator.pushNamed(context, Song.id);},
           icon: Icon(Icons.arrow_back_ios_rounded),
         ),
@@ -32,123 +31,7 @@ class Category extends StatelessWidget {
       ),
       body:
       SingleChildScrollView(
-        child: Column(
-            children:<Widget> [
-              Padding(
-                padding: EdgeInsets.only(top: 20.0,),
-                child: Container(
-                  height: 300,
-                  width: 250,
-                  decoration: BoxDecoration(
-
-                      image: DecorationImage(
-                        image: AssetImage('images/default_img.jpg'),
-                      ),
-                      borderRadius: BorderRadius.circular(20)
-                  ),
-
-                ),
-              ),
-              Column(
-                children: [
-                  Text('Infinity ', style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    color: darkfontcolor
-                  ),),
-
-                  SizedBox(height: 5,),
-                  Text('One Direction',style: TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold, color: darkfontcolor
-                  ),
-
-                  ),
-                ],
-              ) ,
-              //boxs
-              SizedBox(height: 30,),
-              Column(
-                children:<Widget> [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: IconButton(
-                            icon: Icon(Icons.favorite_outline ,color: navigationbariconcolor, ),
-                            onPressed: () {  },
-                          ),
-                        ),
-                        SizedBox(width: 30,),
-                        Text('Like', style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w600, color: darkfontcolor
-                        ), )
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: IconButton(
-                            icon: Icon(Icons.album ,color: navigationbariconcolor, ),
-                            onPressed: () { Navigator.pushNamed(context, Artist.id);},
-                          ),
-                        ),
-                        SizedBox(width: 30,),
-                        Text('View artist', style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w600, color: darkfontcolor
-                        ), )
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: IconButton(
-                            icon: Icon(Icons.share_outlined ,color: navigationbariconcolor , ),
-                            onPressed: () {  },
-                          ),
-                        ),
-                        SizedBox(width: 30,),
-                        Text('Share Your Profile', style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w600, color: darkfontcolor
-                        ), )
-                      ],
-                    ),
-                  ),
-
-                  SizedBox(height: 20,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: IconButton(
-                            icon: Icon(Icons.headset,color: navigationbariconcolor, ),
-                            onPressed: () {  Navigator.pushNamed(context, Playlist.id);},
-                          ),
-                        ),
-                        SizedBox(width: 30,),
-                        Text('Add to Playlist', style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w600, color: darkfontcolor
-                        ), )
-                      ],
-                    ),
-                  ),
-
-
-                ],
-              ),
-
-
-            ]
-        ),
+        child: buildCategory(image: "images/default_img.jpg", song_name: Provider.of<Data>(context,listen: false).Songname, artist_name: Provider.of<Data>(context,listen: false).ArtistName),
       ),
 
 
@@ -156,5 +39,6 @@ class Category extends StatelessWidget {
 
     );
   }
+
 }
 
